531222506206265b5811f5930653fed4cd9c4b9e
hYdrago1 <vanyagoncha@gmail.com>
2025-07-09 16:05:53 +0200

    Initial Commit

---
+++ Traffic light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic light
+//
+//  Created by Ivan Honcharov on 09.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
