e60a25b2b5e0ef4d7438fa8cc2ddfaf86beb248c
hYdrago1 <vanyagoncha@gmail.com>
2025-07-09 16:16:56 +0200

    Delete standart comments

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  Traffic light
-//
-//  Created by Ivan Honcharov on 09.07.25.
-//
+
 
 import UIKit
 
@@ -11,7 +6,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
